c4f73a8223c41424427a46866b1adcea38e0fef1
mayaraolivindo21 <mayaraolivindo21@gmail.com>
2022-03-07 23:17:17 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmovie/ProjetoCriadoNaSemanaDoSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCriadoNaSemanaDoSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCriadoNaSemanaDoSpringReactApplication.class, args);
+	}
+
+}
