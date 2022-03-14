0a4a32aa9a7d2bf0bedaca3cedab223a4605b818
mayaraolivindo21 <mayaraolivindo21@gmail.com>
2022-03-14 00:27:41 -0300

    corrigido01

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -33,14 +33,7 @@ function Listing() {
     const handlePageChange = (newPageNumber: number)=>{
         setPageNumber(newPageNumber);
     }
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-
+  
     return (
         <>
 
