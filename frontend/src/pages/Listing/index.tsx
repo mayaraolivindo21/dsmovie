c29e68180c0a672d3f488a29f95f24a4e5c035df
mayaraolivindo21 <mayaraolivindo21@gmail.com>
2022-03-13 21:31:15 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing(){
+ //forma errada
+ axios.get('http://localhost:8080/movies?size=128page=0')
+ .then(response =>{
+console.log(response.data);
 
+ });
 
     return(
     <>
