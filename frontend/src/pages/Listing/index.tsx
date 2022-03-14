ef620971d8fe64683991a51f6234a261d88313bd
mayaraolivindo21 <mayaraolivindo21@gmail.com>
2022-03-13 23:44:42 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -23,13 +23,16 @@ function Listing() {
         }
     )
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
             });
     }, [pageNumber]);
-
+     
+    const handlePageChange = (newPageNumber: number)=>{
+        setPageNumber(newPageNumber);
+    }
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -41,7 +44,7 @@ function Listing() {
     return (
         <>
 
-            <Pagination />
+            <Pagination page = {page} onChange={handlePageChange}/>
 
             <div className="container">
 
